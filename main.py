cab61c9211b1a49e200f3b428ce4a1d1796e7046
CodeCommitPowerUser-at-408080708256 <EnnovarAWScodeCommit@Wichita.edu>
2020-04-12 12:15:17 -0500

    Updated main.py for SQLlite examples, Added a launch.json for debugging in Visual Studio Code Added a DemoTable with one record entry into the database

--- main.py
+++ main.py
@@ -8,5 +8,35 @@ from pprint import pprint
 # This command is idempotent
 my_db = PySQLLite("database/WSU_AL.db")
 
+try:
+    # Create the table, this command is NOT idempotent
+    # https://www.w3schools.com/sql/sql_datatypes.asp
+    my_db.execute(
+    """CREATE TABLE DemoTable (
+        id INTEGER PRIMARY KEY,
+        description VARCHAR(5)
+        );
+    """
+    )
+except Exception as e:
+    if e.args[0] != 'table DemoTable already exists':
+        raise e
+
+# # Insert data into our demo table
+# # This commands is idempotent
+my_db.execute(
+  """
+    INSERT INTO DemoTable VALUES (
+      1, "Our first DemoTable record entry"
+    );
+  """
+)
+
+# Fetch the data 
+my_db.execute("SELECT * FROM DemoTable")
+
+# Pretty print the fetched data
+pprint(my_db.fetchall())
+
 # Remember to save + close
 my_db.exit()
