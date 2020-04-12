4926b1eee2b264e94d5c70a4ae4e4a2680a4203a
CodeCommitPowerUser-at-408080708256 <EnnovarAWScodeCommit@Wichita.edu>
2020-04-12 11:50:05 -0500

    created directory structure, created the WSU_AL SQLlite database, added phase-I project documentation, added the initial pysqllite module

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello world!')
+"""
+Tutorial: https://repl.it/talk/learn/How-to-create-an-SQLite3-database-in-Python-3/15755
+"""
+from modules.pysqllite import PySQLLite
+from pprint import pprint
+
+# Connect to a database, otherwise create it
+# This command is idempotent
+my_db = PySQLLite("database/WSU_AL.db")
+
+# Remember to save + close
+my_db.exit()
