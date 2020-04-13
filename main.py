ff4fc75b17574af455a33883b31042a1c64cefb6
CodeCommitPowerUser-at-408080708256 <EnnovarAWScodeCommit@Wichita.edu>
2020-04-13 17:18:18 -0500

    Added input statement loop into main to promt user, going to test with repl to poc db persistence

--- main.py
+++ main.py
@@ -4,39 +4,19 @@ Tutorial: https://repl.it/talk/learn/How-to-create-an-SQLite3-database-in-Python
 from modules.pysqllite import PySQLLite
 from pprint import pprint
 
-# Connect to a database, otherwise create it
-# This command is idempotent
-my_db = PySQLLite("database/WSU_AL.db")
 
-try:
-    # Create the table, this command is NOT idempotent
-    # https://www.w3schools.com/sql/sql_datatypes.asp
-    my_db.execute("""CREATE TABLE DemoTable (
-        id INTEGER PRIMARY KEY,
-        description VARCHAR(5)
-        );
-    """)
-except Exception as e:
-    if 'already exists' not in e.args[0]:
-        raise e
+while True:
+    # Connect to a database
+    my_db = PySQLLite("database/WSU_AL.db")
+    
+    statement = input("Please enter a statement: ")
+    my_db.execute(statement)
+    
+    # Fetch the data
+    my_db.execute("SELECT * FROM DemoTable")
 
-try:
-    # Insert data into our demo table
-    # This commands is NOT idempotent
-    my_db.execute("""
-      INSERT INTO DemoTable VALUES (
-        1, "Our first DemoTable record entry"
-      );
-    """)
-except Exception as e:
-    if 'UNIQUE constraint failed' not in e.args[0]:
-        raise e
+    # Pretty print the fetched data
+    pprint(my_db.fetchall())
 
-# Fetch the data
-my_db.execute("SELECT * FROM DemoTable")
-
-# Pretty print the fetched data
-pprint(my_db.fetchall())
-
-# Remember to save + close
-my_db.exit()
+    # Remember to save + close
+    my_db.exit()
