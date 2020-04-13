9c5d994b107c5c783e0fa2cc208d7a6c54eadb66
CodeCommitPowerUser-at-408080708256 <EnnovarAWScodeCommit@Wichita.edu>
2020-04-12 20:26:52 -0500

    flake8 enforcement

--- modules/pysqllite.py
+++ modules/pysqllite.py
@@ -6,31 +6,31 @@ import sqlite3
 
 class PySQLLite(object):
 
-  def __init__(self, database):
-    self.conn = sqlite3.connect(database)
-    self.cursor = self.conn.cursor()
+    def __init__(self, database):
+      self.conn = sqlite3.connect(database)
+      self.cursor = self.conn.cursor()
 
-  def __enter__(self):
-    return self
+    def __enter__(self):
+      return self
 
-  def __exit__(self, exc_type, exc_val, exc_tb):
-    self.conn.close()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+      self.conn.close()
 
-  def execute(self, statement):
-    result = self.cursor.execute(statement)
-    return result
-    try:
+    def execute(self, statement):
       result = self.cursor.execute(statement)
+      return result
+      try:
+        result = self.cursor.execute(statement)
+        self.conn.commit()
+
+      except Exception as e:
+        result = e
+
+      return result
+
+    def fetchall(self):
+      return self.cursor.fetchall()
+
+    def exit(self):
       self.conn.commit()
-
-    except Exception as e:
-      result = e
-
-    return result
-
-  def fetchall(self):
-    return self.cursor.fetchall()
-
-  def exit(self):
-    self.conn.commit()
-    self.conn.close()
+      self.conn.close()
